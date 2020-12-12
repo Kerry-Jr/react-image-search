cb21613a14d9122b48e95f506ab25aa9a4ccdc40
Kerry Smith <kerrysfs@gmail.com>
2020-12-11 22:07:10 -0800

    refactored and use ghost API key to keep repo safe

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
+import apiKey from "../apiKey";
 
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization:
-       'Client-ID **get your own key from unsplash.com/developer'
+       `Client-ID ${apiKey}`
+    // Please go to unsplash.com/developer to get your own key!
   }
 })
